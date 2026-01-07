0ffafcd81e199e2ebb815423412a0e284b7a5b37
soham2yu <sohampande23010@gmail.com>
2026-01-08 00:31:02 +0530

    Downgrade Tailwind to v3 for shadcn compatibility

--- client/tailwind.config.ts
+++ client/tailwind.config.ts
@@ -1,18 +1,10 @@
 import type { Config } from "tailwindcss";
-import animate from "tailwindcss-animate";
-import typography from "@tailwindcss/typography";
 
 export default {
   darkMode: "class",
-  content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {
-      borderRadius: {
-        lg: ".5625rem",
-        md: ".375rem",
-        sm: ".1875rem",
-      },
-    },
+    extend: {},
   },
-  plugins: [animate, typography],
+  plugins: [],
 } satisfies Config;
